6ae334e93d0566a9b112aa3749b62d829e261f13
Plamena Miteva <plamenamiteva@abv.bg>
2015-09-16 17:39:21 +0300

    DB System Overview Homework

---
+++ Databese Systems Overview Homework/10. SQL Language.sql
@@ -0,0 +1,17 @@
+Problem 10. SQL Language
+
+
+SQL is a special-purpose programming language designed for managing data held in a relational database management system (RDBMS), 
+or for stream processing in a relational data stream management system (RDSMS).
+
+Originally based upon relational algebra and tuple relational calculus, SQL consists of a data definition language and a data manipulation language. 
+The scope of SQL includes data insert, query, update and delete, schema creation and modification, and data access control. 
+Although SQL is often described as, and to a great extent is, a declarative language, it also includes procedural elements.
+
+
+SQL query example
+
+SELECT *
+ FROM  Stident
+ WHERE grade > 5
+ ORDER BY name;
